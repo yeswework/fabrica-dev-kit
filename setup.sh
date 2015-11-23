6542a01035d3366c558af82bd503e7325438f90a
Andrew Staffell <andrew@yeswework.com>
2015-11-23 16:43:34 +0100

    comment corrections

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# =========================================================================================
 # YWW WP Vagrant dev kit setup script
-# make sure site.yml is completed with site-specific details before running this
+# =========================================================================================
+# IMPORTANT: make sure site.yml is completed with site-specific details before running this
+# =========================================================================================
 
 # clone YWW WP skeleton theme
 git clone https://andrewstaffell@bitbucket.org/yeswework/yww-wp-skeleton-theme.git dev/src
@@ -18,7 +21,7 @@ bower install
 # start vagrant
 vagrant up
 
-# run our gulp install task which will compile the theme, start vagrant, then activate the theme in WordPress
+# run our gulp install task which will activate the theme in WordPress
 # we let Gulp do it because it needs to read the theme name from the site.yml file
 gulp install
 
@@ -29,7 +32,7 @@ cd ../www
 npm install
 
 # after which, the site will be ready to run and develop locally
-# for development, you'll want to run the following processes:
+# during development, run the following processe simultaneously:
 # gulp watch (in the dev folder)
 # vagrant ssh -c “cd /vagrant/www && gulp browser-sync”
 # see README.md for more
