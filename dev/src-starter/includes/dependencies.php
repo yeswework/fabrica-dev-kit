4af140c7975f87a3acba29d0d5eb9f7031155ba6
João Campos <joao.campos@codingskills.com>
2015-12-08 11:02:28 +0000

    Remove spaces between brackets for dependencies.php

--- dev/src-starter/includes/dependencies.php
+++ dev/src-starter/includes/dependencies.php
@@ -4,41 +4,41 @@
    ========================================================================= */
 
 // Autoload any Composer dependencies
-if( file_exists( 'vendor/autoload.php' ) ) include_once( 'vendor/autoload.php' );
+if(file_exists('vendor/autoload.php')) include_once('vendor/autoload.php');
 
 // Timber: https://github.com/jarednova/timber/
-if ( !class_exists( 'Timber' ) ) {
+if (!class_exists('Timber')) {
 
-	if( is_admin() ) {
+	if(is_admin()) {
 
-		add_action( 'admin_notices', function() {
+		add_action('admin_notices', function() {
 			echo '<div class="error"><p>Error: theme requires <strong>Timber</strong> plugin to be installed and activated.</p></div>';
-		} );
+		});
 
 	} else {
 
-		die( 'Error: Timber not installed.' );
+		die('Error: Timber not installed.');
 
 	}
 
 } else {
 
-	Timber::$dirname = array( 'views' );
+	Timber::$dirname = array('views');
 
 }
 
 // Advanced Custom Fields: http://www.advancedcustomfields.com/
-if ( !class_exists( 'acf' ) ) {
+if (!class_exists('acf')) {
 
-	if( is_admin() ) {
+	if(is_admin()) {
 
-		add_action( 'admin_notices', function() {
+		add_action('admin_notices', function() {
 			echo '<div class="error"><p>Error: theme requires <strong>Advanced Custom Fields</strong> plugin to be installed and activated.</p></div>';
-		} );
+		});
 
 	} else {
 
-		die( 'Error: Advanced Custom Fields not installed.' );
+		die('Error: Advanced Custom Fields not installed.');
 
 	}
 
