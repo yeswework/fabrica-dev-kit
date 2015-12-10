5355c26ab23e2fc47dfdec204ee05e9370fe23c6
Andrew Staffell <andrew@yeswework.com>
2015-12-10 07:42:03 +0100

    change .no-js class to .js on page load

--- dev/src-starter/assets/js/main.js
+++ dev/src-starter/assets/js/main.js
@@ -1,9 +1,12 @@
-( function($) {
+// Replace html.no-js class with .js
+document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, 'js');
+
+(function($) {
 
 	/*
-	$.post(wp_ajax.ajaxurl, { action: 'ACTION' }, function( data ) {
+	$.post(wp_ajax.ajaxurl, {action: 'ACTION'}, function(data) {
 		// results come back in JSON, format data.property
-	}, 'json' ); 
+	}, 'json'); 
 	*/
 
-} ( jQuery ) );
+}(jQuery));
