65ab8a3309f7ce3bb1f1873e0d4d72ade370221a
João Campos <joaovilhenacampos@gmail.com>
2020-02-11 18:10:54 +0100

    fix 'logs' npm script string escape characters

--- dev/package.json.js
+++ dev/package.json.js
@@ -12,7 +12,7 @@ module.exports = (settings) => `
     "sh": "docker-compose exec -u www-data wp /bin/bash",
     "shroot": "docker-compose exec wp /bin/bash",
     "wp": "docker-compose exec -u www-data -T wp wp",
-    "logs": "docker-compose logs -f --tail='46' wp | sed -e \"s/ WARNING: .* into stderr: \\\"\\(NOTICE: PHP message: \\)\\{0,1\\}\\(\\[[a-zA-Z0-9 :\\-]\\{1,\\}\\]\\)\\{0,1\\} \\{0,1\\}\\(.*\\)\\\"/ ⚠️  $(tput setaf 3)\\3$(tput sgr0)/\"",
+    "logs": "docker-compose logs -f --tail='46' wp | sed -e \\"s/ WARNING: .* into stderr: \\\\\\"\\\\(NOTICE: PHP message: \\\\)\\\\{0,1\\\\}\\\\(\\\\[[a-zA-Z0-9 :\\\\-]\\\\{1,\\\\}\\\\]\\\\)\\\\{0,1\\\\} \\\\{0,1\\\\}\\\\(.*\\\\)\\\\\\"/ ⚠️  $(tput setaf 3)\\\\3$(tput sgr0)/\\"",
     "remove": "docker-compose down --volumes --rmi local"
   },
   "fabrica_dev_kit": {
